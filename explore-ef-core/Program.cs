362aa2c1bd0f0ebe0cef7d6a568de000fbb82fad
Shaobo Jiao <shaobojiao@outlook.com>
2024-04-07 11:11:23 +0800

    test Expression

--- explore-ef-core/Program.cs
+++ explore-ef-core/Program.cs
@@ -1,3 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 using Northwind.EntityModels;
 
+Func<Product, bool> pred1 = p => p.Cost >= 50;
+Expression<Func<Product, bool>> pred2 = p => p.Cost >= 50;
+using NorthwindContext context = new();
+IQueryable<Product>? products = context.Products?.Where(pred2);
